42a8d6a03532119378b46d57e04c472a35ee8aa7
tomomi ogino <s4tm83@gmail.com>
2020-11-09 15:03:41 +0900

    お問い合わせ機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_04_104842) do
+ActiveRecord::Schema.define(version: 2020_11_09_043543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2020_11_04_104842) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "content"
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
