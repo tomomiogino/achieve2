5fd47285a0b7bb0d90ddf78fbe54a4b308028f6f
tomomi ogino <s4tm83@gmail.com>
2020-12-04 21:33:50 +0900

    rails gコマンドの際にいらないファイルが生成されないように設定

--- config/application.rb
+++ config/application.rb
@@ -9,5 +9,9 @@ module Sample
     config.load_defaults 5.2
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
   end
 end
